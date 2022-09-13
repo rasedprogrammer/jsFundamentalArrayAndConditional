04428ff65bd561f3390b686928ec0d8634cf52ba
Md Rased Molla <rasedprogrammer@gmail.com>
2022-09-13 12:34:00 +0600

    adding pop & push

---
+++ push-pop.js
@@ -0,0 +1,16 @@
+﻿var numbers = [45, 654, 6, 3, 6, 34, 32];
+// using push to add array last element
+numbers.push(33);
+console.log(numbers);
+var friends = ["Rifat", "Tito", "Farhad"];
+friends.push("Setu");
+friends.push("Naeem");
+console.log(friends);
+// using pop to remove array last element
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
+// Preserve Pop Element In Variable
+var element = numbers.pop();
+console.log(numbers);
+console.log(element);
