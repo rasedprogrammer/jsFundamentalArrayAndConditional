da4fbd62c45c0b9834daa99be1cdad9f919c827c
Md Rased Molla <rasedprogrammer@gmail.com>
2022-09-13 12:13:33 +0600

    adding index and array file

--- array.js
+++ array.js
@@ -1,1 +1,6 @@
-﻿
+﻿var friendAge = [25, 26, 30, 18, 21, 23, 19, 20];
+var priceFee = [200, 400, 600, 300, 900];
+var actorNames = ["Arjun", "Rased", "Shiddhard", "Apurba"];
+var vowels = ["a", "e", "i", "o", "u"];
+console.log(actorNames);
+console.log(actorNames.length);
---
+++ index.js
@@ -0,0 +1,14 @@
+﻿var numbers = [20, 49, 56, 34, 54, 67, 89];
+// Get Element Value By Index
+console.log(numbers[2]);
+console.log(numbers);
+console.log(numbers[0]);
+console.log(numbers[5]);
+var elements = numbers[3];
+console.log(elements);
+// Set Element Value By Index
+numbers[3] = 66;
+console.log(numbers);
+// Find Index Number Of An Element
+var positionIndex = numbers.indexOf(66, 89);
+console.log(positionIndex);
