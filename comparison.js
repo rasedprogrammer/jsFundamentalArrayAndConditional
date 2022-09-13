c12611926301f384d090afa758806be1eff84167
Md Rased Molla <rasedprogrammer@gmail.com>
2022-09-13 13:34:16 +0600

    adding conditional and comparison

--- comparison.js
+++ comparison.js
@@ -1,1 +1,33 @@
-﻿console.log(5 < 6);
+﻿// Boto Choto Kina ber Korte Hole
+console.log(5 < 6);
+console.log(5 > 6);
+// Soman Soman Kina Ber Korte Hole
+console.log(5 == 6);
+console.log(6 == 6);
+// Osoman Kina Ber Korte Hole
+console.log(5 != 6);
+console.log(6 != 6);
+// Less Than Or Equal
+console.log(5 <= 6);
+console.log(6 <= 6);
+console.log(15 <= 6);
+// Getar Than Or Equal
+console.log(5 >= 6);
+console.log(6 >= 6);
+console.log(15 >= 6);
+
+// Comparison Check With Variable
+var num1 = 33;
+var num2 = 66;
+
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 === num2);
+console.log(num1 !== num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+
+// Muliple Conditions
+
+// money1 > money2 && result1 > result2 && car1 > car2;
+// money1 > money2 || result1 > result2 || car1 > car2;
---
+++ conditional.js
@@ -0,0 +1,20 @@
+﻿var iPhonPrice = 80000;
+var myBudget = 9000;
+// if iphone price is less than my budget than i will buy the iphone
+if (iPhonPrice < myBudget) {
+  console.log("I Will Buy The Iphone");
+} else {
+  console.log("I Have Not Enough Money To Buy Iphone");
+}
+var chickenPrice = 300;
+var myPokectBalance = 90;
+
+if (chickenPrice <= myPokectBalance) {
+  console.log("I Will Buy The Chicken");
+} else if (chickenPrice > myPokectBalance) {
+  console.log("I Am Going To Buy Smashed Potato And Lentils Soup");
+} else {
+  console.log(
+    "I Have Not Enough Money To Buy Chicken Or Smashed Potato And Lentils Soup"
+  );
+}
